ef6ead395b04bf78d379ff76a8848470e2516752
jatintyagi98 <jatintyagi97@gmail.com>
2021-11-08 13:49:16 +0000

    added genres to UI

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 
 import './App.css';
 import Navbar from './Components/Navbar';
-import Banner from './Components/Banner';
-import Movies from './Components/Movies';
+// import Banner from './Components/Banner';
+// import Movies from './Components/Movies';
+import Favourites from './Components/Favourites';
 
 function App() {
   return (
     <>
 
     <Navbar/>
-    <Banner/>
-    <Movies/>
+    {/* <Banner/>
+    <Movies/> */}
+    <Favourites/>
+    
     
 
     </>
